c725305fdb82a06ce568fe5404fdf009083a8443
Daniel Huffman <drhuffman12@gmail.com>
2025-07-06 22:33:54 -0400

    reupdate version

--- src/ai4cr2/about.cr
+++ src/ai4cr2/about.cr
@@ -1,8 +1,8 @@
 # TODO: Write documentation for `Ai4cr2::Guard`
 module Ai4cr2
-  module Guard
-    VERSION = "0.2.0"
+  module About
+    VERSION = "0.2.1a"
   end
 
-  p Ai4cr2::Guard::VERSION
+  p Ai4cr2::About::VERSION
 end
